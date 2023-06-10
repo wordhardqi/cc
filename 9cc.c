cd276b1cd98fb8c4df902a54d8b1ff8a7878da4c
rmq <rmqi@live.com>
2023-06-10 11:31:04 +0800

    add error at

--- 9cc.c
+++ 9cc.c
@@ -5,24 +5,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-typedef enum {
+typedef enum
+{
 	TK_RESERVED,
 	TK_NUM,
 	TK_EOF,
 } TokenKind;
 
-typedef struct  Token
+typedef struct Token
 {
 	TokenKind kind;
-	struct Token * next;
+	struct Token *next;
 	int val;
 	char *str;
 } Token;
 
+char *uesr_input;
 Token *token;
 
-void error(char *fmt, ...) {
+void error(char *fmt, ...)
+{
 	va_list ap;
 	va_start(ap, fmt);
 	vfprintf(stderr, fmt, ap);
@@ -30,85 +32,110 @@ void error(char *fmt, ...) {
 	exit(1);
 }
 
-bool consume(char op) {
-	if(token->kind != TK_RESERVED || token->str[0] !=op)
+void error_at(char *loc, char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+
+	int pos = loc - uesr_input;
+	fprintf(stderr, "%s\n", uesr_input);
+	fprintf(stderr, "%*s", pos, " ");
+	fprintf(stderr, "^ ");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
+bool consume(char op)
+{
+	if (token->kind != TK_RESERVED || token->str[0] != op)
 		return false;
 	token = token->next;
 	return true;
 }
 
-void expect(char op) {
-	if(token->kind != TK_RESERVED || token->str[0] !=op)
-		error("expected '%c'", op);
+void expect(char op)
+{
+	if (token->kind != TK_RESERVED || token->str[0] != op)
+		error_at(token->str, "expected '%c'", op);
 	token = token->next;
 }
 
-int expect_number() {
-	if(token->kind != TK_NUM)
-		error("expected a number");
+int expect_number()
+{
+	if (token->kind != TK_NUM)
+		error_at(token->str, "expected a number");
 	int val = token->val;
 	token = token->next;
 	return val;
 }
 
-bool at_eof() {
+bool at_eof()
+{
 	return token->kind == TK_EOF;
 }
 
-Token *new_token(TokenKind kind, Token *cur, char *str){
+Token *new_token(TokenKind kind, Token *cur, char *str)
+{
 	Token *tok = calloc(1, sizeof(Token));
 	tok->kind = kind;
-	tok->str =str;
-	cur->next  = tok;
+	tok->str = str;
+	cur->next = tok;
 	return tok;
 }
 
-Token *tokenize(char *p){
+Token *tokenize(char *p)
+{
 	Token head;
 	head.next = NULL;
 	Token *cur = &head;
-	while(*p) {
-		if(isspace(*p)){
+	while (*p)
+	{
+		if (isspace(*p))
+		{
 			p++;
 			continue;
 		}
-		if(*p == '+' || *p == '-'){
+		if (*p == '+' || *p == '-')
+		{
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
-		if(isdigit(*p)){
-			cur = new_token(TK_NUM,cur,p);
+		if (isdigit(*p))
+		{
+			cur = new_token(TK_NUM, cur, p);
 			cur->val = strtol(p, &p, 10);
 			continue;
 		}
-		error("invalid token");
+		error_at(p, "invalid token");
 	}
-	new_token(TK_EOF, cur,p);
+	new_token(TK_EOF, cur, p);
 	return head.next;
 }
 
-
-
-int main(int argc, char **argv) {
-	if (argc !=2) {
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
 		error("%s: invalid number of arguments", argv[0]);
 	}
-
-	token = tokenize(argv[1]);
-
+	uesr_input = argv[1];
+	token = tokenize(uesr_input);
 
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
 	printf("  mov rax, %d\n", expect_number());
-	while(!at_eof()){
-		if(consume('+')){
+	while (!at_eof())
+	{
+		if (consume('+'))
+		{
 			printf("  add rax, %d\n", expect_number());
 			continue;
 		}
 		expect('-');
-		printf("  sub rax, %d\n",expect_number());
+		printf("  sub rax, %d\n", expect_number());
 	}
 	printf("  ret\n");
-	return 0; 
+	return 0;
 }
