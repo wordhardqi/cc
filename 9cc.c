bf55adada25215addd53e7a1f7fa3794b8771857
rmq <rmq@bytedance.com>
2023-06-10 13:02:20 +0800

    change format

--- 9cc.c
+++ 9cc.c
@@ -7,17 +7,17 @@
 
 typedef enum
 {
-	TK_RESERVED,
-	TK_NUM,
-	TK_EOF,
+    TK_RESERVED,
+    TK_NUM,
+    TK_EOF,
 } TokenKind;
 
 typedef struct Token
 {
-	TokenKind kind;
-	struct Token *next;
-	int val;
-	char *str;
+    TokenKind kind;
+    struct Token *next;
+    int val;
+    char *str;
 } Token;
 
 char *uesr_input;
@@ -25,117 +25,117 @@ Token *token;
 
 void error(char *fmt, ...)
 {
-	va_list ap;
-	va_start(ap, fmt);
-	vfprintf(stderr, fmt, ap);
-	fprintf(stderr, "\n");
-	exit(1);
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
 }
 
 void error_at(char *loc, char *fmt, ...)
 {
-	va_list ap;
-	va_start(ap, fmt);
+    va_list ap;
+    va_start(ap, fmt);
 
-	int pos = loc - uesr_input;
-	fprintf(stderr, "%s\n", uesr_input);
-	fprintf(stderr, "%*s", pos, " ");
-	fprintf(stderr, "^ ");
-	vfprintf(stderr, fmt, ap);
-	fprintf(stderr, "\n");
-	exit(1);
+    int pos = loc - uesr_input;
+    fprintf(stderr, "%s\n", uesr_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
 }
 
 bool consume(char op)
 {
-	if (token->kind != TK_RESERVED || token->str[0] != op)
-		return false;
-	token = token->next;
-	return true;
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        return false;
+    token = token->next;
+    return true;
 }
 
 void expect(char op)
 {
-	if (token->kind != TK_RESERVED || token->str[0] != op)
-		error_at(token->str, "expected '%c'", op);
-	token = token->next;
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        error_at(token->str, "expected '%c'", op);
+    token = token->next;
 }
 
 int expect_number()
 {
-	if (token->kind != TK_NUM)
-		error_at(token->str, "expected a number");
-	int val = token->val;
-	token = token->next;
-	return val;
+    if (token->kind != TK_NUM)
+        error_at(token->str, "expected a number");
+    int val = token->val;
+    token = token->next;
+    return val;
 }
 
 bool at_eof()
 {
-	return token->kind == TK_EOF;
+    return token->kind == TK_EOF;
 }
 
 Token *new_token(TokenKind kind, Token *cur, char *str)
 {
-	Token *tok = calloc(1, sizeof(Token));
-	tok->kind = kind;
-	tok->str = str;
-	cur->next = tok;
-	return tok;
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
 }
 
 Token *tokenize(char *p)
 {
-	Token head;
-	head.next = NULL;
-	Token *cur = &head;
-	while (*p)
-	{
-		if (isspace(*p))
-		{
-			p++;
-			continue;
-		}
-		if (*p == '+' || *p == '-')
-		{
-			cur = new_token(TK_RESERVED, cur, p++);
-			continue;
-		}
-		if (isdigit(*p))
-		{
-			cur = new_token(TK_NUM, cur, p);
-			cur->val = strtol(p, &p, 10);
-			continue;
-		}
-		error_at(p, "invalid token");
-	}
-	new_token(TK_EOF, cur, p);
-	return head.next;
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+    while (*p)
+    {
+        if (isspace(*p))
+        {
+            p++;
+            continue;
+        }
+        if (*p == '+' || *p == '-')
+        {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+        if (isdigit(*p))
+        {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+        error_at(p, "invalid token");
+    }
+    new_token(TK_EOF, cur, p);
+    return head.next;
 }
 
 int main(int argc, char **argv)
 {
-	if (argc != 2)
-	{
-		error("%s: invalid number of arguments", argv[0]);
-	}
-	uesr_input = argv[1];
-	token = tokenize(uesr_input);
+    if (argc != 2)
+    {
+        error("%s: invalid number of arguments", argv[0]);
+    }
+    uesr_input = argv[1];
+    token = tokenize(uesr_input);
 
-	printf(".intel_syntax noprefix\n");
-	printf(".globl main\n");
-	printf("main:\n");
-	printf("  mov rax, %d\n", expect_number());
-	while (!at_eof())
-	{
-		if (consume('+'))
-		{
-			printf("  add rax, %d\n", expect_number());
-			continue;
-		}
-		expect('-');
-		printf("  sub rax, %d\n", expect_number());
-	}
-	printf("  ret\n");
-	return 0;
+    printf(".intel_syntax noprefix\n");
+    printf(".globl main\n");
+    printf("main:\n");
+    printf("  mov rax, %d\n", expect_number());
+    while (!at_eof())
+    {
+        if (consume('+'))
+        {
+            printf("  add rax, %d\n", expect_number());
+            continue;
+        }
+        expect('-');
+        printf("  sub rax, %d\n", expect_number());
+    }
+    printf("  ret\n");
+    return 0;
 }
